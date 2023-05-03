eca0833969a2d6d1262335223e9c5b356a76c6c1
bigyanbhandari <bigyanbhandari01@gmail.com>
2023-05-03 14:32:56 +0545

    finished

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,17 @@ import why1 from "../assests/why1.png";
 import why2 from "../assests/why2.png";
 import why3 from "../assests/why3.png";
 import why4 from "../assests/why4.jpg";
+import f1 from "../assests/f1.JPG";
+import f2 from "../assests/f2.jpg";
+import f3 from "../assests/f3.jpeg";
+import f4 from "../assests/f4.jpeg";
+import f5 from "../assests/f5.JPG";
+import f6 from "../assests/f6.jpg";
+import f7 from "../assests/f7.jpeg";
+import f8 from "../assests/f8.JPG";
+import img1 from "../assests/img1.jpg";
+import img2 from "../assests/img2.jpg";
+import img3 from "../assests/img3.jpg";
 
 const AboutUs = () => {
   return (
@@ -34,7 +45,14 @@ const AboutUs = () => {
           <br />
           <strong className="mt-2">- Rudolf Steiner</strong>
         </p>
-        <div>iimmages</div>
+        
+        <div className="flex gap-3 border-8 border-indigo-500/75 w-[50%] ml-[350px] rounded-full   ">
+          
+          <img  src={img1} dir="ltr" className="h-[250px] w-[250px] object-cover border  rounded-s-full  "/>
+          <img src={img2} className="h-[250px] w-[250px] object-cover "/>
+          <img src={img3} dir="rtl" className="h-[250px] w-[250px] object-cover  rounded-s-full"/>
+        </div>
+        
         <div>
           <span className="text-3xl font-bold text-gray-800 p-4 m-4 flex justify-center">
             Why Himalaya Waldorf School?
@@ -101,36 +119,114 @@ const AboutUs = () => {
             </p>
           </div>
         </div>
-        <div>
-        <div className="w-[40%] ">
-          <span className="text-3xl font-bold text-gray-800 p-4 m-4 ">
-            FACILITIES
-          </span>
-          <p className="text-justify p-4 m-4">
-            <strong> Library: </strong>We have a well-stocked library with all
-            of the necessary texts and a wide range of books.There is a library
-            that meets the needs of each grade.
-            <br /> <br />
-            <strong>ECA and CCA:</strong> Our school provides holistic
-            education, prioritizing both ECA and CCA activities equally. We
-            organize athletics, cultural programs, and other activities for our
-            students.
-            <br /> <br />
-            <strong>Transportation :</strong> Transportation for students is
-            provided from important pick up points. Cafeteria : Our school's
-            cafeteria serves three healthy meals in a clean environment,
-            ensuring students have access to healthy food choices.
-            <br /> <br />
-            <strong>Outside play ground : </strong> Himalaya waldorf Waldorf
-            School has a spacious playground where children get all the
-            opportunities for developing their goals, motoric, social and
-            physical skills.
-          </p>
+        <div className="flex">
+          <div className="w-[40%] ">
+            <span className="text-3xl font-bold text-gray-800 p-4 m-4 ">
+              FACILITIES
+            </span>
+            <p className="text-justify p-4 m-4">
+              <strong> Library: </strong>We have a well-stocked library with all
+              of the necessary texts and a wide range of books.There is a
+              library that meets the needs of each grade.
+              <br /> <br />
+              <strong>ECA and CCA:</strong> Our school provides holistic
+              education, prioritizing both ECA and CCA activities equally. We
+              organize athletics, cultural programs, and other activities for
+              our students.
+              <br /> <br />
+              <strong>Transportation :</strong> Transportation for students is
+              provided from important pick up points. Cafeteria : Our school's
+              cafeteria serves three healthy meals in a clean environment,
+              ensuring students have access to healthy food choices.
+              <br /> <br />
+              <strong>Outside play ground : </strong> Himalaya waldorf Waldorf
+              School has a spacious playground where children get all the
+              opportunities for developing their goals, motoric, social and
+              physical skills.
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex gap-4 mt-12">
+              <img
+                src={f1}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+              <img
+                src={f2}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+
+              <img
+                src={f3}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+              <img
+                src={f4}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+            </div>
+            <div className="flex gap-4 mt-5">
+              <img
+                src={f5}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+
+              <img
+                src={f6}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+              <img
+                src={f7}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+              <img
+                src={f8}
+                className="h-[200px] w-[200px] object-cover rounded-lg"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-             
+      </div>
+      <div className="mt-10 ">
+        <div className="flex justify-around">
+        <div >
+        <span className="text-3xl font-bold text-gray-800 p-4 m-4 ">
+          FEATURES
+        </span> 
+        <ul className="list-disc text-justify p-4 m-4   ">
+          <li>
+            Focus on the holistic (physical, emotional, social, intellectual,
+            spiritual) development of children.
+          </li>
+          <li>Committed and qualified teachers</li>
+          <li>Collaboration between family and institution</li>
+          <li>Social and athletic training</li>
+          <li>Focus on behavioral attitude</li>
+          <li>Conducive learning environment</li>
+          <li>Relate all learning to the real world</li>
+          <li>Focus on conceptual clarity and creativity, not memorization</li>
+        </ul>
         </div>
+        <div className="w-[400px]">
+        <span className="text-3xl font-bold text-gray-800 p-4 m-4 ">
+          ASSESSMENT
+        </span> 
+        <p className="text-justify p-4 m-4">
+        We assess our students in a variety of ways.We have a continuous, informal assessment 
+system that is used for periodic evaluation and improvement of our children. We evaluate a range of different areas of development such as behavioural, emotional and intellectual aspects of our students.
+
+        </p>
         </div>
+        <div className="w-[400px]">
+        <span className="text-3xl font-bold text-gray-800 p-4 m-4 ">
+         SCHOOL TIME
+        </span> 
+        <p className="text-justify p-4 m-4">
+        We are open from 9.00 AM to 3.30 PM. Additional time will be provided for children if necessary.
+
+        </p>
+        </div>
+      </div>
       </div>
     </div>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { GoLocation } from "react-icons/go";
+import { TbMessageShare } from "react-icons/tb";
+import { IoIosCall } from "react-icons/io";
+import reading from "../assests/reading.jpg";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="flex items-center">
+        <div className="flex flex-col">
+          <span className="text-5xl font-bold  p-4 m-4 ">Contact Us</span>
+          <span className="p-4 m-4 text-xl text-justify text-gray-800">
+            Himalaya Waldorf School is here to provide you with more
+            information,
+            <br />
+            answer any inquiry. For any inquiries, please contact to the given
+            number below and <br />
+            we will endeavor to respond as quickly as possible.
+          </span>
+        </div>
+        <div>
+          <img src={reading} className="h-[300px]" />
+        </div>
+      </div>
+      <div>
+        <ul className="  p-4 m-4  ml-[800px] text-gray-500">
+          <li className="flex items-center gap-4 text-xl  ">
+            <GoLocation size={30} />
+            Nagarjun, Kathmandu, Nepal
+          </li>
+          <li className="flex items-center gap-4 text-xl">
+            {" "}
+            <TbMessageShare size={30} />
+            himalayawaldorf@gmail.com
+          </li>
+          <li className="flex items-center gap-4 text-xl">
+            {" "}
+            <IoIosCall size={30} />
+            9851109795
+          </li>
+          <li className="flex items-center gap-4 text-xl">
+            {" "}
+            <IoIosCall size={30} />
+            985-1141885
+          </li>
+        </ul>
+      </div>
+      <div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.298879495522!2d85.28575217534767!3d27.738925976164545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19e1826d298f%3A0x4112951b5e244b94!2sHimalaya%20Waldorf%20School!5e0!3m2!1sen!2snp!4v1683103416749!5m2!1sen!2snp"
+          width="900"
+          height="450"
+          
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          className="ml-[250px] p-6 "
+        ></iframe>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
